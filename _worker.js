5833e665d7ba26f4a9d740f90f62fb5997f12182
amzayo <55936606+amzayo@users.noreply.github.com>
2023-09-24 11:06:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,18 @@
 export default {
-    async fetch(request, env) {
-      let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
-        url.hostname="gn.cfsaas.ltd";
-        let new_request=new Request(url,request);
-        return fetch(new_request);
-      }
-      // Otherwise, serve the static assets.
-      return env.ASSETS.fetch(request);
-    }
-  };
+async fetch(request, env) {
+let url = new URL(request.url);
+if (url.pathname.startsWith('/')) {
+url.hostname = getRandomHostname();
+let new_request = new Request(url, request);
+return fetch(new_request);
+}
+// Otherwise, serve the static assets.
+return env.ASSETS.fetch(request);
+}
+};
+
+function getRandomHostname() {
+const hostnames = ["104.16.96.8", "172.67.128.75", "104.24.192.3"];
+const randomIndex = Math.floor(Math.random() * hostnames.length);
+return hostnames[randomIndex];
+}
